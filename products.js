4c5189e62f19b2104a551ba7e19d033153cba618
Dale Corns <codefellow@gmail.com>
2018-07-24 20:32:34 -0700

    Initial commit

---
+++ products.js
@@ -0,0 +1,43 @@
+/**
+ * products
+ * Created by dcorns on 7/24/18
+ * Copyright © 2018 Dale Corns
+ * MIT Licensed
+ */
+'use strict';
+
+exports.handler = (event, context, callback) => {
+
+  let id =  (event.pathParameters || {}).product || false;
+  switch(event.httpMethod){
+
+    case "GET":
+
+      if(id) {
+        callback(null, {body: "This is a READ operation on product ID " + id});
+        return;
+      }
+
+      callback(null, {body: "This is a LIST operation, return all products"});
+      break;
+
+    case "POST":
+      callback(null, {body: "This is a CREATE operation"});
+      break;
+
+    case "PUT":
+      callback(null, {body: "This is an UPDATE operation on product ID " + id});
+      break;
+
+    case "DELETE":
+      callback(null, {body:"This is a DELETE operation on product ID " + id});
+      break;
+
+    default:
+      // Send HTTP 501: Not Implemented
+      console.log("Error: unsupported HTTP method (" + event.httpMethod + ")");
+      callback(null, { statusCode: 501 });
+
+  }
+
+};
---
+++ readme.md
@@ -0,0 +1,4 @@
+#aws-sam
+A simple example of using aws sam run AWS lambda locally.
+
+**template.yaml**: This is used to create the local lambda server. It creates endpoints for running the functions in products.js . By default, the start-api and invoke commands search working directory for the template.yaml file.
